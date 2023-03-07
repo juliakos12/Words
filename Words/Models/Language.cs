29878798c8c96525170d6380c3009a88d5d71d61
Julia <juliakos498@gmai.com>
2023-03-07 23:40:33 +0200

    Added models

---
+++ Words/Models/Language.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace words.Models
+{
+    internal class Language
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public List<Word> Words { get; set; }
+
+        public Language(Guid id, string title, List<Word> words)
+        {
+            Id = id;
+            Title = title;
+            Words = words;
+        }
+
+        public double getAverageLevel()
+        {
+            return Words.Sum(x => x.Level) / Words.Count;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} : {getAverageLevel()}";
+        }
+    }
+}
---
+++ Words/Models/Word.cs
@@ -0,0 +1,23 @@
+﻿namespace words.Models
+{
+    internal class Word
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Translation { get; set; }
+        public float Level { get; set; }
+
+        public Word(Guid id, string title, string translation, float level)
+        {
+            Id = id;
+            Title = title;
+            Translation = translation;
+            Level = level;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Translation}";
+        }
+    }
+}
