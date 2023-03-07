d87139884a2b5d82511bffcb5df866c62b27369d
Julia <juliakos498@gmai.com>
2023-03-08 00:16:24 +0200

    added level Tostring method

--- Words/Models/Word.cs
+++ Words/Models/Word.cs
@@ -1,4 +1,10 @@
-﻿namespace words.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace words.Models
 {
     internal class Word
     {
