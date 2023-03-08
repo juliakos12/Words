acc05598e01e30c8dd57e514c946528333c3361a
Julia <juliakos498@gmai.com>
2023-03-09 01:27:17 +0200

    Fixed models

--- Words/Models/Language.cs
+++ Words/Models/Language.cs
@@ -11,28 +11,24 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
+        public String Description { get; set; }
 
-        public Language(Guid id, string title, List<Word> words)
+        public Language(Guid id, string title, List<Word> words, string description)
         {
             Id = id;
             Title = title;
             Words = words;
+            Description = description;
         }
 
         public double getAverageLevel()
         {
-            double sum = 0;
-            foreach(var word in Words)
-            {
-                sum += word.Level;
-            }
-
-            return sum / Words.Count;
+            return Words.Sum(x => x.Level) / Words.Count;
         }
 
         public override string ToString()
         {
-            return Title;
+            return $"{Title} : {getAverageLevel()}";
         }
     }
 }
